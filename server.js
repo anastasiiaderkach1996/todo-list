4533cc5374c5c3d5adc27f853e2f67feb2f18bab
anastasiiaderkach1996 <lgg16@me.com>
2019-11-01 14:53:37 +0200

    Added mongodb

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID
-var db = require('./db')
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+let db; 
 
-let elementsFromToDoList = [];
+// let elementsFromToDoList = [];
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,20 +15,18 @@ MongoClient.connect('mongodb://localhost:27017',
     { useUnifiedTopology: true },
     function(err,database){
     if (err) {
-       return console.log(err);
+       console.log(err);
     }
     db = database.db('mytodolist');
     // db.createCollection('customers',function (err, res) {
     //     if (err) throw err;
-    //         return console.log(err); 
     //     console.log('colection created');
-    //     client.close();
-        
     // }) 
     app.listen(3012, function() {
         console.log('API app started');
       })
 })
+
 
 app.get('/', function (req, res) {
     res.sendFile('main.html', {root: __dirname + '/public'});
@@ -37,13 +34,22 @@ app.get('/', function (req, res) {
 })
 
 app.post('/api/todo', (req, res) => {
-    const filteredElementsFromToDo = elementsFromToDoList.filter( el => el == req.body.item);
-     if (filteredElementsFromToDo.length == 0) {
-        elementsFromToDoList.push(req.body.item);
-        res.send('Item Added')
-    } else {
-        res.send('Already added')
-        
-    }
+    // const filteredElementsFromToDo = elementsFromToDoList.filter( el => el == req.body.item);
+    //  if (filteredElementsFromToDo.length == 0) {
+    //     elementsFromToDoList.push(req.body.item);
+    const listItem = req.body;
+    const customers = db.collection('customers');
+    customers.find().toArray(function (err, result) {
+        const filterResult = result.filter(el => el.item == req.body.item);
+        console.log(filterResult, result)
+        if (filterResult.length == 0){
+            db.collection('customers').insertOne(listItem, err => {
+                if (err) throw err;
+                res.send('Item Added')
+            })
+        } else {
+            res.send('Already added')
+        }
+    })
 })
 
